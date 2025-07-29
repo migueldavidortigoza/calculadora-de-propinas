7c7c884f7c992e9c557b6fb4787dc19792114635
Miguel David Ortigoza <migueldavidortigoza@gmail.com>
2025-07-29 10:10:30 -0300

    subo proyecto calculadora de propinas

---
+++ app.js
@@ -0,0 +1,16 @@
+function calcularPropinas() {
+    const monto =
+    parseFloat(document.getElementById("monto").value);
+    const porcentaje = parseFloat(document.getElementById("porcentaje").value);
+
+    if (isNaN(monto) || isNaN(porcentaje)) {
+        alert("Por favor, ingresa valores validos.");
+        return;
+    }
+
+    const propina = (monto * porcentaje) / 100;
+    const total = monto + propina;
+
+    document.getElementById("resultado").textContent = `Propina: $${propina.toFixed(2)} | Total: $${total.toFixed(2)}`;
+    
+}
